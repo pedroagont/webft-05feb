6ed5682c2068b95451b6789021607cc2a168072d
Pedro A. González <pgonzalez@post.com>
2024-02-27 14:40:20 -0500

    Add jquery code notes

--- w04d2/app.js
+++ w04d2/app.js
@@ -1,58 +1,98 @@
-console.log('hello from app.js!');
+$(document).ready(() => {
+  console.log('hello from app.js!');
 
-// DOM
-console.log(navigator);
-console.log(window);
-console.log(document);
+  // DOM
+  console.log(navigator);
+  console.log(window);
+  console.log(document);
 
-// --------- HOW TO SELECT ELEMENTS
-// console.log(document.getElementById('header-title'));
+  // --------- HOW TO SELECT ELEMENTS
+  // console.log(document.getElementById('header-title'));
 
-const header = document.getElementById('header');
-const headerTitle = document.getElementById('header-title');
-const headerDescription = document.getElementById('header-description');
-const todoList = document.getElementById('todo-list');
-const form = document.getElementById('form');
-const formSubmit = document.getElementById('form-submit');
+  // with vanilla js
+  // const header = document.getElementById('header');
+  // const headerTitle = document.getElementById('header-title');
+  // const headerDescription = document.getElementById('header-description');
+  // const todoList = document.getElementById('todo-list');
+  // const form = document.getElementById('form');
+  // const formSubmit = document.getElementById('form-submit');
 
-// --------- HOW TO CHANGE CONTENT
-headerTitle.innerHTML = 'Hello from app.js! 👋';
+  // with jquery
+  const header = $('#header');
+  const todoList = $('#todo-list');
+  const form = $('#form');
 
-// --------- HOW TO CHANGE STYLES
-header.style.background = 'lightgreen';
-header.style.color = 'black';
+  // --------- HOW TO CHANGE CONTENT
+  // with vanilla js
+  // headerTitle.innerHTML = 'Hello from app.js! 👋';
 
-// --------- HOW TO DYNAMIC RENDERING
-const todosData = ['learn html', 'learn css', 'learn js'];
+  // with jquery
+  $('#header-title').html('Hello from app.js! 👋');
 
-console.log(todoList);
+  // --------- HOW TO CHANGE STYLES
+  // with vanilla js
+  // header.style.background = 'lightgreen';
+  // header.style.color = 'black';
 
-const renderTodos = () => {
-  todoList.innerHTML = '';
+  // with jquery
+  header.css('background', 'lightgreen');
+  header.css('color', 'black');
 
-  for (const todo of todosData) {
-    const todoMarkup = `<article>
-        <h4>${todo}</h4>
-      </article>`;
-    todoList.innerHTML += todoMarkup;
-  }
-};
+  // --------- HOW TO DYNAMIC RENDERING
+  const todosData = ['learn html', 'learn css', 'learn js'];
 
-renderTodos();
+  console.log(todoList);
 
-// --------- HOW TO ACCESS TO EVENTS
-// document.addEventListener('click', () => {
-//   console.log('document clicked!');
-// });
+  const renderTodos = () => {
+    // with vanilla js
+    //   todoList.innerHTML = '';
+    //   for (const todo of todosData) {
+    //     const todoMarkup = `<article>
+    //         <h4>${todo}</h4>
+    //       </article>`;
+    //     todoList.innerHTML += todoMarkup;
+    //   }
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const { content } = event.target.elements;
-
-  todosData.push(content.value);
-
-  content.value = '';
+    // with jquery
+    todoList.empty();
+    for (const todo of todosData) {
+      const todoMarkup = `<article>
+          <h4>${todo}</h4>
+        </article>`;
+      todoList.append(todoMarkup);
+    }
+  };
 
   renderTodos();
+
+  // --------- HOW TO ACCESS TO EVENTS
+  // document.addEventListener('click', () => {
+  //   console.log('document clicked!');
+  // });
+
+  // with vanilla js
+  // form.addEventListener('submit', (event) => {
+  //   event.preventDefault();
+
+  //   const { content } = event.target.elements;
+
+  //   todosData.push(content.value);
+
+  //   content.value = '';
+
+  //   renderTodos();
+  // });
+
+  // with jquery
+  form.submit((event) => {
+    event.preventDefault();
+
+    const { content } = event.target.elements;
+
+    todosData.push(content.value);
+
+    content.value = '';
+
+    renderTodos();
+  });
 });
