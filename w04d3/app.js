bc5c9cbfe821fc6f51b70c67916baa04947bb64e
Pedro A. González <pgonzalez@post.com>
2024-02-28 14:35:34 -0500

    Add search form example

--- w04d3/app.js
+++ w04d3/app.js
@@ -31,4 +31,47 @@ $(document).ready(() => {
   };
 
   getAllPokemon();
+
+  const createPokemonSearchResultMarkup = (res) => {
+    const pokemonMarkup = `
+    <article class="search-result">
+      <img src="${res.sprites.other.home.front_default}" alt="">
+      <h4>${res.name}</h4>
+      <p>${res.types[0].type.name}</p>
+    </article>
+    `;
+    return pokemonMarkup;
+  };
+
+  const renderPokemonSearchResult = (res) => {
+    const pokemonMarkup = createPokemonSearchResultMarkup(res);
+    $('#pokemon-list').html(pokemonMarkup);
+  };
+
+  const getPokemonByName = (pokemonName) => {
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+      method: 'GET',
+    })
+      .then((res) => {
+        console.log(res);
+        renderPokemonSearchResult(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        $('#pokemon-list').html('Pokemon not found!');
+      });
+  };
+
+  const handlePokemonSearch = (e) => {
+    e.preventDefault();
+
+    // console.log(e.target.elements.name.value);
+    const pokemonName = e.target.elements.name.value;
+    $(e.target.elements.name).val('');
+
+    getPokemonByName(pokemonName);
+  };
+
+  $('#pokemon-search-form').submit(handlePokemonSearch);
 });
